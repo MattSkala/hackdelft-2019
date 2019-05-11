9907f30ff8f6762fbae0bc20dfefe9a0bc99a265
Shresth Agrawal <shresthagrawal@Shresths-MacBook-Pro.local>
2019-05-11 17:48:03 +0200

    contract: add conditionalToken.sol

---
+++ contracts/conditionalTokens.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract conditionalTokens {
+    struct Company{
+
+    }
+
+    struct Employee{
+
+    }
+
+    struct ServiceProvider{
+
+    }
+
+    mapping(address=>int) balance;
+
+    function addCompany();
+    function refillAccount();
+    function addEmployee();
+    function addServiceProvider();
+    function startTrip();
+    function payServiceProvider();
+    function endTrip();
+    function claimAccount();
+
+    //Other Functions
+    function getEmployees();
+
+
+}
