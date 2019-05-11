b3160ebcd4107db920ce08e97a7c102d4f19516e
Shresth Agrawal <shresthagrawal@Shresths-MacBook-Pro.local>
2019-05-11 19:39:36 +0200

    contract: add claim struct

--- contracts/conditionalTokens.sol
+++ contracts/conditionalTokens.sol
@@ -6,24 +6,50 @@ contract conditionalTokens {
         uint256 balance;
         string name;
     }
-    /*
-    struct Employee{
 
+    struct Claim{
+        address company;
+        uint256 priceLimit;
+        string city;
+        uint startTime;
+        uint endTime;
+    }
+
+    struct Employee{
+        uint8 exists;
+        string name;
+        string homeCity;
+        address payable company;
     }
 
     struct ServiceProvider{
-
+        uint exists;
+        string name;
+        string location;
     }
-    */
+
     mapping(address=>Company) public allCompanies;
+    mapping(address=>Employee) public allEmployee;
+    mapping(address=>address[]) public companyEmployees;
+    mapping(address=>ServiceProvider) public allServiceProviders;
 
     function addCompany(string memory name) public payable{
         require(allCompanies[msg.sender].exists!=1);
         Company memory newCompany = Company(1, msg.value, name);
         allCompanies[msg.sender] = newCompany;
     }
-    /*
-    function refillAccount();
+
+    function refillAccount() public payable{
+        require(allCompanies[msg.sender].exists==1);
+        allCompanies[msg.sender].balance = msg.value;
+    }
+
+    function addServiceProvider(string memory name, string memory location) public payable{
+        require(allServiceProviders[msg.sender].exists!=1);
+        ServiceProvider memory newServiceProvider = ServiceProvider(1,name,location);
+        allServiceProviders[msg.sender] = newServiceProvider;
+    }
+   /*
     function addEmployee();
     function addServiceProvider();
     function startTrip();
