080f90ebb6ae9716b6a91bb516983719ad1cdd12
Shresth <shresthagrawal.31@gmail.com>
2019-05-11 22:41:25 +0200

    contract: Add events on transfer and claim

--- contracts/conditionalTokens.sol
+++ contracts/conditionalTokens.sol
@@ -35,6 +35,8 @@ contract conditionalTokens {
     mapping(address=>ServiceProvider) public allServiceProviders;
     mapping(address=>Claim) public allClaims;
 
+    event transfer(address indexed company, address indexed employeeAddress, address serviceProvider, uint amount);
+    event claim(address indexed company, address indexed employeeAddress, uint256 budget, string city, uint startTime, uint endTime);
     function addCompany(string memory name) public payable{
         require(allCompanies[msg.sender].exists!=1);
         Company memory newCompany = Company(1, msg.value, name);
@@ -63,6 +65,7 @@ contract conditionalTokens {
     function addClaim(address employeeAddress, string memory city, uint256 priceLimit, uint startTime, uint endTime) public {
         require(allCompanies[msg.sender].exists==1);
         require(allEmployees[employeeAddress].exists==1);
+        emit claim(msg.sender, employeeAddress, priceLimit, city, startTime, endTime);
         Claim memory newClaim = Claim(1, msg.sender, priceLimit, city, startTime, endTime);
         allClaims[employeeAddress] = newClaim;
     }
@@ -73,6 +76,7 @@ contract conditionalTokens {
         require(allServiceProviders[serviceProvider].exists==1);
         require(!strCmp(allClaims[msg.sender].city, allEmployees[msg.sender].homeCity) && strCmp(allClaims[msg.sender].city, allServiceProviders[serviceProvider].city));
         require(allClaims[msg.sender].budget>amount);
+        emit transfer(allEmployees[msg.sender].company, msg.sender, serviceProvider, amount);
         serviceProvider.transfer(amount);
     }
 
